e51573367cf62f5b4208bfce6273b930b13c4d67
isty03 <114085968+isty03@users.noreply.github.com>
2022-12-18 21:40:10 +0800

    Add files via upload

---
+++ struktur data/stack dan queue.js
@@ -0,0 +1,18 @@
+var buah = ["mangga"];
+buah.push("alpukat");
+buah.push("anggur");
+buah.push("durian");
+buah.push("apel");
+    buah.pop();
+    console.log(buah);
+
+var hewan = ["singa"];
+hewan.push("kucing");
+hewan.push("kelinci");
+hewan.push("sapi");
+hewan.push("anjing");
+    hewan.shift();
+    console.log(hewan);
+
+// Nama : Isty Hamdayani
+// NIM : H071211039
